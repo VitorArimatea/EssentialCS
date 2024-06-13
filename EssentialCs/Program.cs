4e2bbec899fe9be9c14e08bbbe2db89b84d12fae
Vitor Arimatea <vitorodev@outlook.com>
2024-06-13 12:37:39 -0300

    Adicionar arquivos de projeto.

---
+++ EssentialCs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialCs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int qualquerSoma = 1 + 4 - 7;
+            Console.WriteLine(qualquerSoma);
+
+            Console.ReadLine();
+        }
+    }
+}
